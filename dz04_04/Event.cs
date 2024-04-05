0a5839b3e13b98d467a62ad24b31559a037219ab
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-05 18:51:58 +0300

    done

--- dz04_04/Event.cs
+++ dz04_04/Event.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+namespace dz04_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonacciChecker checker = new FibonacciChecker();
+            checker.FibonacciChecked += OnFibonacciChecked;
+
+            int number = 21;
+
+            Console.WriteLine($"Checking if {number} is a Fibonacci number...");
+            checker.CheckFibonacci(number);
+        }
+
+        static void OnFibonacciChecked(object sender, FibonacciCheckEventArgs e)
+        {
+            if (e.IsFibonacci)
+            {
+                Console.WriteLine($"{e.Number} is a Fibonacci number.");
+            }
+            else
+            {
+                Console.WriteLine($"{e.Number} is not a Fibonacci number.");
+            }
+        }
+    }
+}
--- dz04_04/Program.cs
+++ dz04_04/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿using System;
+
+namespace dz04_04
+{
+    public static class IntExtensions
+    {
+        public static bool IsFibonacci(this int number)
+        {
+            double sqrt5 = Math.Sqrt(5);
+            double phi = (1 + sqrt5) / 2;
+            double psi = (1 - sqrt5) / 2;
+
+            int fib1 = (int)Math.Round(Math.Pow(phi, number) / sqrt5);
+            int fib2 = (int)Math.Round(Math.Pow(psi, number) / sqrt5);
+
+            return fib1 * fib1 == number || fib2 * fib2 == number;
+        }
+    }
+
+    public class FibonacciChecker
+    {
+        public event EventHandler<FibonacciCheckEventArgs> FibonacciChecked;
+
+        public void CheckFibonacci(int number)
+        {
+            bool isFibonacci = number.IsFibonacci();
+            OnFibonacciChecked(new FibonacciCheckEventArgs(number, isFibonacci));
+        }
+
+        protected virtual void OnFibonacciChecked(FibonacciCheckEventArgs e)
+        {
+            FibonacciChecked?.Invoke(this, e);
+        }
+    }
+
+    public class FibonacciCheckEventArgs : EventArgs
+    {
+        public int Number { get; }
+        public bool IsFibonacci { get; }
+
+        public FibonacciCheckEventArgs(int number, bool isFibonacci)
+        {
+            Number = number;
+            IsFibonacci = isFibonacci;
+        }
+    }
+}
