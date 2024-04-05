696896600685b75324072754b3e90c6249986a71
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-05 18:54:56 +0300

    done

--- dz04_04/Event.cs
+++ dz04_04/Event.cs
@@ -1,1 +1,56 @@
-﻿
+﻿using System;
+
+namespace dz04_04
+{
+    public static class StringExtensions
+    {
+        public static event EventHandler<WordCountEventArgs> WordCountCompleted;
+
+        public static int CountWords(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return 0;
+
+            int count = 0;
+            bool inWord = false;
+
+            foreach (char c in str)
+            {
+                if (char.IsWhiteSpace(c))
+                {
+                    if (inWord)
+                    {
+                        count++;
+                        inWord = false;
+                    }
+                }
+                else
+                {
+                    inWord = true;
+                }
+            }
+
+            if (inWord)
+                count++;
+
+            OnWordCountCompleted(count);
+
+            return count;
+        }
+
+        private static void OnWordCountCompleted(int count)
+        {
+            WordCountCompleted?.Invoke(null, new WordCountEventArgs(count));
+        }
+    }
+
+    public class WordCountEventArgs : EventArgs
+    {
+        public int WordCount { get; }
+
+        public WordCountEventArgs(int wordCount)
+        {
+            WordCount = wordCount;
+        }
+    }
+}
--- dz04_04/Program.cs
+++ dz04_04/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+namespace dz04_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Це приклад рядка для підрахунку слів";
+
+            int wordCount = text.CountWords();
+            Console.WriteLine($"The number of words in the string is: {wordCount}");
+        }
+    }
+}
