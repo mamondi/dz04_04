80e454ef99d53be03e1824c13a80d4d93bd25afe
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-05 18:57:52 +0300

    done

--- dz04_04/Event.cs
+++ dz04_04/Event.cs
@@ -1,1 +1,58 @@
-﻿
+﻿using System;
+
+namespace dz04_04
+{
+    public static class StringExtensions
+    {
+        public static event EventHandler<LastWordLengthEventArgs> LastWordLengthCalculated;
+
+        public static int GetLastWordLength(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return 0;
+
+            int length = 0;
+            int lastWordStartIndex = -1;
+            bool inWord = false;
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                if (char.IsWhiteSpace(str[i]))
+                {
+                    if (inWord)
+                    {
+                        lastWordStartIndex = i + 1;
+                        break;
+                    }
+                }
+                else
+                {
+                    inWord = true;
+                }
+            }
+
+            if (lastWordStartIndex != -1)
+            {
+                length = str.Length - lastWordStartIndex;
+                OnLastWordLengthCalculated(length);
+            }
+
+            return length;
+        }
+
+        private static void OnLastWordLengthCalculated(int length)
+        {
+            LastWordLengthCalculated?.Invoke(null, new LastWordLengthEventArgs(length));
+        }
+    }
+
+    public class LastWordLengthEventArgs : EventArgs
+    {
+        public int LastWordLength { get; }
+
+        public LastWordLengthEventArgs(int lastWordLength)
+        {
+            LastWordLength = lastWordLength;
+        }
+    }
+}
--- dz04_04/Program.cs
+++ dz04_04/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+namespace dz04_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "ляляляляляля останнє слово -> dorov";
+
+            int lastWordLength = text.GetLastWordLength();
+            Console.WriteLine($"The length of the last word in the string is: {lastWordLength}");
+        }
+    }
+}
