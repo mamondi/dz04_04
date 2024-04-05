02274cdcc0ecdb4875cea887afc333e7e39f656c
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-05 19:03:19 +0300

    done

--- dz04_04/Event.cs
+++ dz04_04/Event.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+
+namespace dz04_04
+{
+    public static class ArrayExtensions
+    {
+        public static event EventHandler<ArrayFilteredEventArgs> ArrayFiltered;
+
+        public static int[] Filter(this int[] array, Predicate<int> predicate)
+        {
+            if (array == null || predicate == null)
+                throw new ArgumentNullException();
+
+            int[] filteredArray = Array.FindAll(array, predicate);
+
+            OnArrayFiltered(filteredArray);
+
+            return filteredArray;
+        }
+
+        private static void OnArrayFiltered(int[] filteredArray)
+        {
+            ArrayFiltered?.Invoke(null, new ArrayFilteredEventArgs(filteredArray));
+        }
+    }
+
+    public class ArrayFilteredEventArgs : EventArgs
+    {
+        public int[] FilteredArray { get; }
+
+        public ArrayFilteredEventArgs(int[] filteredArray)
+        {
+            FilteredArray = filteredArray;
+        }
+    }
+}
--- dz04_04/Program.cs
+++ dz04_04/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System;
+
+namespace dz04_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            ArrayExtensions.ArrayFiltered += OnArrayFiltered;
+
+            int[] filteredEven = numbers.Filter(x => x % 2 == 0);
+
+            Console.WriteLine("Filtered even numbers:");
+            foreach (int num in filteredEven)
+            {
+                Console.WriteLine(num);
+            }
+
+            int[] filteredOdd = numbers.Filter(x => x % 2 != 0);
+
+            Console.WriteLine("\nFiltered odd numbers:");
+            foreach (int num in filteredOdd)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        static void OnArrayFiltered(object sender, ArrayFilteredEventArgs e)
+        {
+            Console.WriteLine($"\nFiltered array: [{string.Join(", ", e.FilteredArray)}]");
+        }
+    }
+}
