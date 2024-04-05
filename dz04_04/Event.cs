831a87f80479b0bf19d62c4b55c4b5069fac4355
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-05 19:00:24 +0300

    done

--- dz04_04/Event.cs
+++ dz04_04/Event.cs
@@ -1,1 +1,73 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace dz04_04
+{
+    public static class StringExtensions
+    {
+        public static event EventHandler<BracketsValidationEventArgs> BracketsValidationCompleted;
+
+        public static bool IsValidBrackets(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return true;
+
+            Stack<char> bracketsStack = new Stack<char>();
+            bool isValid = true;
+
+            foreach (char c in str)
+            {
+                if (IsOpeningBracket(c))
+                {
+                    bracketsStack.Push(c);
+                }
+                else if (IsClosingBracket(c))
+                {
+                    if (bracketsStack.Count == 0 || !IsMatchingPair(bracketsStack.Pop(), c))
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+
+            isValid &= bracketsStack.Count == 0;
+
+            OnBracketsValidationCompleted(isValid);
+
+            return isValid;
+        }
+
+        private static bool IsOpeningBracket(char c)
+        {
+            return c == '(' || c == '[' || c == '{';
+        }
+
+        private static bool IsClosingBracket(char c)
+        {
+            return c == ')' || c == ']' || c == '}';
+        }
+
+        private static bool IsMatchingPair(char opening, char closing)
+        {
+            return (opening == '(' && closing == ')') ||
+                   (opening == '[' && closing == ']') ||
+                   (opening == '{' && closing == '}');
+        }
+
+        private static void OnBracketsValidationCompleted(bool isValid)
+        {
+            BracketsValidationCompleted?.Invoke(null, new BracketsValidationEventArgs(isValid));
+        }
+    }
+
+    public class BracketsValidationEventArgs : EventArgs
+    {
+        public bool IsValid { get; }
+
+        public BracketsValidationEventArgs(bool isValid)
+        {
+            IsValid = isValid;
+        }
+    }
+}
--- dz04_04/Program.cs
+++ dz04_04/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+//повністю чатгпт
+namespace dz04_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = "{}[]";
+            string input2 = "(())";
+            string input3 = "[{}]";
+            string input4 = "[}";
+            string input5 = "[[{]}]";
+
+            Console.WriteLine($"Is '{input1}' valid: {input1.IsValidBrackets()}");
+            Console.WriteLine($"Is '{input2}' valid: {input2.IsValidBrackets()}");
+            Console.WriteLine($"Is '{input3}' valid: {input3.IsValidBrackets()}");
+            Console.WriteLine($"Is '{input4}' valid: {input4.IsValidBrackets()}");
+            Console.WriteLine($"Is '{input5}' valid: {input5.IsValidBrackets()}");
+        }
+    }
+}
