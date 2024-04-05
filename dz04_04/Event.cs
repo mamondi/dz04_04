6a35cdbea804ecbcae4dfb7abae17bd9e0a52673
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-05 19:05:23 +0300

    done

--- dz04_04/Event.cs
+++ dz04_04/Event.cs
@@ -1,1 +1,60 @@
-﻿
+﻿using System;
+
+namespace dz04_04
+{
+    public class DayTemperature
+    {
+        private int[] temperatures;
+
+        public event EventHandler<MaxTemperatureDifferenceEventArgs> MaxTemperatureDifferenceFound;
+
+        public DayTemperature(int[] temperatures)
+        {
+            this.temperatures = temperatures;
+        }
+
+        public void FindDaysWithMaxTemperatureDifference()
+        {
+            int maxTemperatureDifference = 0;
+            int[] daysWithMaxDifference = new int[0];
+
+            for (int i = 0; i < temperatures.Length; i++)
+            {
+                for (int j = i + 1; j < temperatures.Length; j++)
+                {
+                    int difference = Math.Abs(temperatures[i] - temperatures[j]);
+                    if (difference > maxTemperatureDifference)
+                    {
+                        maxTemperatureDifference = difference;
+                        daysWithMaxDifference = new int[] { i, j };
+                    }
+                    else if (difference == maxTemperatureDifference)
+                    {
+                        Array.Resize(ref daysWithMaxDifference, daysWithMaxDifference.Length + 2);
+                        daysWithMaxDifference[daysWithMaxDifference.Length - 2] = i;
+                        daysWithMaxDifference[daysWithMaxDifference.Length - 1] = j;
+                    }
+                }
+            }
+
+            OnMaxTemperatureDifferenceFound(maxTemperatureDifference, daysWithMaxDifference);
+        }
+
+        private void OnMaxTemperatureDifferenceFound(int maxDifference, int[] days)
+        {
+            MaxTemperatureDifferenceFound?.Invoke(this, new MaxTemperatureDifferenceEventArgs(maxDifference, days));
+        }
+    }
+
+    public class MaxTemperatureDifferenceEventArgs : EventArgs
+    {
+        public int MaxDifference { get; }
+        public int[] Days { get; }
+
+        public MaxTemperatureDifferenceEventArgs(int maxDifference, int[] days)
+        {
+            MaxDifference = maxDifference;
+            Days = days;
+        }
+    }
+}
--- dz04_04/Program.cs
+++ dz04_04/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+
+namespace dz04_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] temperatures = { 20, 25, 30, 15, 10, 25, 35 };
+
+            DayTemperature dayTemperature = new DayTemperature(temperatures);
+            dayTemperature.MaxTemperatureDifferenceFound += OnMaxTemperatureDifferenceFound;
+            dayTemperature.FindDaysWithMaxTemperatureDifference();
+        }
+
+        static void OnMaxTemperatureDifferenceFound(object sender, MaxTemperatureDifferenceEventArgs e)
+        {
+            Console.WriteLine($"Maximum temperature difference: {e.MaxDifference} °C");
+            Console.WriteLine("Days with maximum temperature difference:");
+            foreach (int day in e.Days)
+            {
+                Console.WriteLine($"Day {day + 1}: {temperatures[day]} °C");
+            }
+        }
+    }
+}
