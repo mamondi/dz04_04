3cb9c288aa55625fc5ffd7e7ef8b092f5a20337f
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-05 19:09:53 +0300

    done

--- dz04_04/Event.cs
+++ dz04_04/Event.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+using System.Linq;
+
+namespace dz04_04
+{
+    public delegate void MaxAndAverageGradesEventHandler(string subjectWithMaxGrade, int maxGrade, int averageGrade);
+
+    public class StudentGrades
+    {
+        private readonly string[] subjects;
+        private readonly int[] grades;
+
+        public event MaxAndAverageGradesEventHandler MaxAndAverageGradesEvent;
+
+        public StudentGrades(string[] subjects, int[] grades)
+        {
+            if (subjects.Length != grades.Length)
+                throw new ArgumentException("The number of subjects must match the number of grades.");
+
+            this.subjects = subjects;
+            this.grades = grades;
+        }
+
+        public void CalculateMaxAndAverageGrades()
+        {
+            int maxGradeIndex = Array.IndexOf(grades, grades.Max());
+            int averageGrade = (int)grades.Average();
+
+            MaxAndAverageGradesEvent?.Invoke(subjects[maxGradeIndex], grades[maxGradeIndex], averageGrade);
+        }
+    }
+}
--- dz04_04/Program.cs
+++ dz04_04/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+//теж чисто чатгпт
+namespace dz04_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] subjects = { "Math", "Physics", "Chemistry", "Biology" };
+            int[] grades = { 90, 85, 75, 80 };
+
+            StudentGrades student = new StudentGrades(subjects, grades);
+            student.MaxAndAverageGradesEvent += (subjectWithMaxGrade, maxGrade, averageGrade) =>
+            {
+                Console.WriteLine($"Subject with the highest grade: {subjectWithMaxGrade}");
+                Console.WriteLine($"Max Grade: {maxGrade}");
+                Console.WriteLine($"Average Grade: {averageGrade}");
+            };
+
+            student.CalculateMaxAndAverageGrades();
+        }
+    }
+}
